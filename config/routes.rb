be33cfb53e8d598b4588541cc2052a5e76017fd1
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-09 16:58:50 +0100

    add routes, redirection to courses index, generate controller courses, view index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
   devise_for :teachers
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  resource :teachers, only: [:show, :edit, :update]
+
+  resources :courses do
+    resources :students, only: [:new, :create, :edit, :update]
+    # aristutor.co/courses/students/42 here we are on student id:42
+    resources :summaries, only: [:new, :create]
+    # aristutor.co/courses/:courses_id/summary/new
+  end
+
+  resources :summaries, only: [:index, :show, :edit, :update] do
+    # aristutor.co/summaries/54 here we are on summary id:54
+    # aristutor.co/summaries/55/edit here we are on summary id:55
+    member do
+       patch :send
+      # aristutor.co/summaries/54/send
+     end
+  end
 end
