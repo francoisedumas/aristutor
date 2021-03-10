f023495becc6452e1c0988d32f1193115a0eaae3
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-10 12:18:36 +0100

    search enabled with pg_search

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,9 @@
 class CoursesController < ApplicationController
+  def index
+    if params[:query].present?
+      @courses = Course.where("teacher_id = ?", current_teacher.id).search_by_first_and_last_name(params[:query])
+    else
+      @courses = Course.where("teacher_id = ?", current_teacher.id)
+    end
+  end
 end
