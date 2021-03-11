e91c5811e41e9d5af2ea2dc3b8c00f6962fbc623
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-11 13:50:30 +0100

    translation begin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,8 @@ class PagesController < ApplicationController
 
   def components
   end
+
+  def translation
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,5 @@ Rails.application.routes.draw do
   end
 
   get '/components', to: 'pages#components'
-  
+  get '/translation', to: 'pages#translation'
 end
