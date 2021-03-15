258d057561d1e415d7d796fb805ba3cd9424c66f
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-15 10:03:49 +0100

    add if to JS script

--- app/javascript/customs/flashcards.js
+++ app/javascript/customs/flashcards.js
@@ -3,17 +3,19 @@ const flipDisplay = () => {
   const studentAnswer = document.getElementById('student-answer');
 
   const flip = document.getElementById('flip');
-  flip.addEventListener('click', (event) => {
-    // get the answer
-    const studentAnswerValue = studentAnswer.value;
-    // display the answer in the hidden answer section
-    const studentAnswerDisplay = document.getElementById('student-answer-display');
-    studentAnswerDisplay.innerText = `Votre reponse: ${studentAnswerValue}`;
-    // hide the question section
-    document.getElementById('question').style.display = 'none';
-    // show the answer section
-    document.getElementById('answer').style.display = 'block';
-  })
+  if (flip) {
+    flip.addEventListener('click', (event) => {
+      // get the answer
+      const studentAnswerValue = studentAnswer.value;
+      // display the answer in the hidden answer section
+      const studentAnswerDisplay = document.getElementById('student-answer-display');
+      studentAnswerDisplay.innerText = `Votre reponse: ${studentAnswerValue}`;
+      // hide the question section
+      document.getElementById('question').style.display = 'none';
+      // show the answer section
+      document.getElementById('answer').style.display = 'block';
+    })
+  }
 }
 
 
