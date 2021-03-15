04aa99481737a69b15af4fbf21d2678094e07ab6
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-15 13:08:56 +0100

    done

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,45 +1,36 @@
 class FlashcardsController < ApplicationController
   def show
     # Selecting the summary from where we create the flashcards
-    @summary = Summary.find(params[:summary_id])
+    # Do an eager loading of all words and mistake on the current summary
+    @summary = Summary.includes(:words, :mistakes).find(params[:summary_id])
 
-    # In order to generate randomly flashcards from both words and mistakes tables
-    # a sample between the Class Word and Mistake is done
-    flashcard_class = [Word, Mistake].sample
+    # Extraction of size, words and mistakes
+    total_words             = @summary.words.size
+    total_mistakes          = @summary.mistakes.size
+    all_words_not_passed    = @summary.words.select {|word| word.status != "passed"}
+    all_mistakes_not_passed = @summary.mistakes.select {|mistake| mistake.status != "passed"}
 
-    total_words = Word.where(summary: @summary).count
-    total_mistakes = Mistake.where(summary: @summary).count
-    total_words_passed = Word.where(summary: @summary).where(status: "passed").count
-    total_mistakes_passed = Mistake.where(summary: @summary).where(status: "passed").count
+    # Creation of a global array with object Word and Mistakes not passed
+    all_flashcards_not_passed = [all_words_not_passed, all_mistakes_not_passed].flatten
 
+    # Calculation
     @number_total = total_words + total_mistakes
-    @number_total_passed = total_words_passed + total_mistakes_passed
-
+    number_total_not_passed = all_words_not_passed.size + all_mistakes_not_passed.size
+    @number_total_passed = @number_total - number_total_not_passed
     @completion = (@number_total_passed * 1.00 / @number_total * 100).to_i
 
     if @number_total_passed < @number_total
-      # Supposing the class randomely selected is Word the first line is
-      # Word.where.not(status: "passed") select only word not passed from table words
-      # .where(summary: @summary) selecting only word with foreign key of our summary
-      # .order('RANDOM()').first then shuffle all those words not passed and select the first one
-      @flashcard = flashcard_class.where.not(status: "passed")
-                                  .where(summary: @summary).order('RANDOM()').first
 
-      # if @flashcard is empty meaning that all the line from word of this summary are passed
-      # we check for the other class Mistake if anything remains to be passed
-
-      unless @flashcard
-        flashcard_class = ([Word, Mistake] - [flashcard_class]).first
-        @flashcard = flashcard_class.where.not(status: "passed")
-                                    .where(summary: @summary).order('RANDOM()').first
-      end
+      # From the global array sample a word or a mistake
+      @flashcard = all_flashcards_not_passed.sample
     else
       redirect_to success_summary_flashcards_path
     end
   end
 
   def passed
-    # generation of the class Word or Mistake
+    # From the view we got the class as a string in the params
+    # Turns it into constante and then chains ActiveRecord methods
     # see it in action using rails c and 'Word'.constantize => give a "class" Word
     # 'application_record'.camelize.constantize =>  give a "class" ApplicationRecord
     klass = params['summary_id'].split('/').last.camelize.constantize
@@ -62,5 +53,4 @@ class FlashcardsController < ApplicationController
 
   def success
   end
-
 end
