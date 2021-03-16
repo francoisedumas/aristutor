16c7edcfd3ad29dae6b3b329234562072bb3804e
yann letertre <yannletertre10@gmail.com>
2021-03-16 15:38:29 +0100

    Commit empty change to re-push branch and fix master

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Aristutor
       generate.test_framework :test_unit, fixture: false
     end
 
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
