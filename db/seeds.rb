d3e55e60f82da4bbc99b9287bc13e45dc7a985b2
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-18 16:32:47 +0100

    change francois in seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 puts "Cleaning Database"
 Mistake.destroy_all
 Word.destroy_all
