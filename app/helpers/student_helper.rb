8a69c50712dd9833b410c1c6d386a39a945b00d2
yann letertre <yannletertre10@gmail.com>
2021-03-18 10:08:55 +0100

    change class to avatar on course show and and index

--- app/helpers/student_helper.rb
+++ app/helpers/student_helper.rb
@@ -1,9 +1,9 @@
 module StudentHelper
-  def student_img(student)
+  def student_img(student, class)
     if student && student.photo.attached?
-      cl_image_tag student.photo.key, class:'small-avatar'
+      cl_image_tag student.photo.key, class: class
     else
-      image_tag 'default-avatar.png', class: "small-avatar"
+      image_tag 'default-avatar.png', class: class
     end
   end
 end
