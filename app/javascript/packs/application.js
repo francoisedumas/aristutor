16704c483b9d39b54a7e6d0edc6b11314ffd5a1b
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-03-25 17:42:04 +0100

    center element

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("controllers/index")
 // External imports
 import "bootstrap";
 import {flipDisplay} from '../customs/flashcards';
+import {carouselSlide} from '../customs/carousel';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -37,5 +38,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   flipDisplay();
+  carouselSlide();
   // canvasConfetti();
 });
